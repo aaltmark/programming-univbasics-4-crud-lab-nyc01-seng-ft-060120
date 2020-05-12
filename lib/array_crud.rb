e4c4f9b07c172e290b4778e0a4cdb4cd5f8e3891
Adam Altmark <adamaltmark@gmail.com>
2020-05-12 12:38:59 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
+adam_sandler_characters = ["Happy Gilmore", "Billy Madison", "Sonny Koufax", "Bobby Boucher"]
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  adam_sandler_characters = ["wow", "Billy Madison", "am", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  wow = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  create_an_array[3]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  create_an_array[2] = "totally"
 end
